dfb818e403b709bc273374661fcf86de742edc4f
doryashar <dor.yashar@gmail.com>
2025-03-03 08:50:23 +0200

    Release version v1.0.8

--- scripts/create_release.sh
+++ scripts/create_release.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 tmpfile=$1; printf "" > "$tmpfile";
-for file in ./src/{common_funcs,config,push_to_stdin,conversations,agent,install,update,main}; do cat "$file" >> "$tmpfile"; echo >> "$tmpfile"; done; 
+for file in ./src/{common_funcs,config,terminal_buffer,push_to_stdin,conversations,agent,install,update,main}; do cat "$file" >> "$tmpfile"; echo >> "$tmpfile"; done; 
--- scripts/test.sh
+++ scripts/test.sh
@@ -2,5 +2,5 @@
 CURR_DIR=$(dirname "$0");
 tmpfile="./tmp/ai" # $(mktemp);
 bash "$CURR_DIR/create_release.sh" "$tmpfile"; 
-bash "$tmpfile" "$@"; 
+AI_DEBUG=1 bash "$tmpfile" "$@"; 
 # rm "$tmpfile"
